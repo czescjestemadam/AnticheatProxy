429729bc48640af849845016416b7d0e944c4e73
adas <adambimek@gmail.com>
2024-07-17 22:02:25 +0200

    add packet

--- src/network/protocol/packet/configuration/c2s/acknowledge_finish_configuration.cc
+++ src/network/protocol/packet/configuration/c2s/acknowledge_finish_configuration.cc
@@ -1,1 +1,35 @@
 #include "acknowledge_finish_configuration.hh"
+
+#include "network/handler/configuration_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::configuration::c2s::AcknowledgeFinishConfiguration::read(const ProtocolVersion* version)
+{
+}
+
+void acp::packet::configuration::c2s::AcknowledgeFinishConfiguration::write(const ProtocolVersion* version)
+{
+}
+
+bool acp::packet::configuration::c2s::AcknowledgeFinishConfiguration::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* configHandler = dynamic_cast<ConfigurationHandler*>(handler.get()))
+		return configHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::configuration::c2s::AcknowledgeFinishConfiguration::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x03;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x02;
+
+	return -1;
+}
+
+std::string acp::packet::configuration::c2s::AcknowledgeFinishConfiguration::toString() const
+{
+	return "AcknowledgeFinishConfiguration";
+}
--- src/network/protocol/packet/configuration/c2s/acknowledge_finish_configuration.hh
+++ src/network/protocol/packet/configuration/c2s/acknowledge_finish_configuration.hh
@@ -1,9 +1,20 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::configuration::c2s
 {
-	class AcknowledgeFinishConfiguration
+	class AcknowledgeFinishConfiguration : public IPacket
 	{
+	public:
+		using IPacket::IPacket;
 
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		std::string toString() const override;
 	};
 }
