28e188934b32313c8d2725fa965de6d17d87efe0
adas <adambimek@gmail.com>
2024-07-18 12:56:58 +0200

    finish configuration packet

--- src/network/protocol/packet/configuration/s2c/finish_configuration.cc
+++ src/network/protocol/packet/configuration/s2c/finish_configuration.cc
@@ -1,1 +1,35 @@
 #include "finish_configuration.hh"
+
+#include "network/handler/configuration_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::configuration::s2c::FinishConfiguration::read(const ProtocolVersion* version)
+{
+}
+
+void acp::packet::configuration::s2c::FinishConfiguration::write(const ProtocolVersion* version)
+{
+}
+
+bool acp::packet::configuration::s2c::FinishConfiguration::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* configHandler = dynamic_cast<ConfigurationHandler*>(handler.get()))
+		return configHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::configuration::s2c::FinishConfiguration::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x03;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x02;
+
+	return -1;
+}
+
+std::string acp::packet::configuration::s2c::FinishConfiguration::toString() const
+{
+	return "FinishConfiguration";
+}
--- src/network/protocol/packet/configuration/s2c/finish_configuration.hh
+++ src/network/protocol/packet/configuration/s2c/finish_configuration.hh
@@ -1,9 +1,20 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::configuration::s2c
 {
-	class FinishConfiguration
+	class FinishConfiguration : public IPacket
 	{
+	public:
+		using IPacket::IPacket;
 
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		std::string toString() const override;
 	};
 }
