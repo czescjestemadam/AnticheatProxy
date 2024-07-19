ad956e6678379f771333f34ccf866a7853e51630
adas <adambimek@gmail.com>
2024-07-19 19:39:38 +0200

    SwingArm packet

--- src/network/protocol/packet/play/c2s/swing_arm.cc
+++ src/network/protocol/packet/play/c2s/swing_arm.cc
@@ -1,1 +1,53 @@
 #include "swing_arm.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::c2s::SwingArm::read(const ProtocolVersion* version)
+{
+	hand = buf.readVarint();
+}
+
+void acp::packet::play::c2s::SwingArm::write(const ProtocolVersion* version)
+{
+	buf.writeVarint(hand);
+}
+
+bool acp::packet::play::c2s::SwingArm::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::c2s::SwingArm::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x36;
+	if (*version >= ProtocolVersion::v1_20_3)
+		return 0x33;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x32;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x2F;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x2E;
+
+	return 0x2C;
+}
+
+int& acp::packet::play::c2s::SwingArm::getHand()
+{
+	return hand;
+}
+
+void acp::packet::play::c2s::SwingArm::setHand(int hand)
+{
+	this->hand = hand;
+}
+
+std::string acp::packet::play::c2s::SwingArm::toString() const
+{
+	return std::format("SwingArm[{}]", hand);
+}
--- src/network/protocol/packet/play/c2s/swing_arm.hh
+++ src/network/protocol/packet/play/c2s/swing_arm.hh
@@ -1,9 +1,29 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::c2s
 {
-	class SwingArm
+	class SwingArm : public IPacket
 	{
+		/**
+		 * - 0: main hand
+		 * - 1: off hand
+		 */
+		int hand;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		int& getHand();
+		void setHand(int hand);
+
+		std::string toString() const override;
 	};
 }
