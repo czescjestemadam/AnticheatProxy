771845c9fa50d2efc003c88582a2de373bfd1b64
adas <adambimek@gmail.com>
2024-07-20 16:58:31 +0200

    SetHealth packet

--- src/network/protocol/packet/play/s2c/set_health.cc
+++ src/network/protocol/packet/play/s2c/set_health.cc
@@ -1,1 +1,81 @@
 #include "set_health.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::SetHealth::read(const ProtocolVersion* version)
+{
+	health = buf.readFloat();
+	food = buf.readVarint();
+	foodSaturation = buf.readFloat();
+}
+
+void acp::packet::play::s2c::SetHealth::write(const ProtocolVersion* version)
+{
+	buf.writeFloat(health);
+	buf.writeVarint(food);
+	buf.writeFloat(foodSaturation);
+}
+
+bool acp::packet::play::s2c::SetHealth::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::s2c::SetHealth::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x5D;
+	if (*version >= ProtocolVersion::v1_20_3)
+		return 0x5B;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x59;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x57;
+	if (*version >= ProtocolVersion::v1_19_3)
+		return 0x53;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x55;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x52;
+
+	return 0x49;
+}
+
+float acp::packet::play::s2c::SetHealth::getHealth() const
+{
+	return health;
+}
+
+void acp::packet::play::s2c::SetHealth::setHealth(const float health)
+{
+	this->health = health;
+}
+
+int acp::packet::play::s2c::SetHealth::getFood() const
+{
+	return food;
+}
+
+void acp::packet::play::s2c::SetHealth::setFood(const int food)
+{
+	this->food = food;
+}
+
+float acp::packet::play::s2c::SetHealth::getFoodSaturation() const
+{
+	return foodSaturation;
+}
+
+void acp::packet::play::s2c::SetHealth::setFoodSaturation(const float food_saturation)
+{
+	foodSaturation = food_saturation;
+}
+
+std::string acp::packet::play::s2c::SetHealth::toString() const
+{
+	return std::format("SetHealth[hp={:.2f}, food={}, sat={:.2f}]", health, food, foodSaturation);
+}
--- src/network/protocol/packet/play/s2c/set_health.hh
+++ src/network/protocol/packet/play/s2c/set_health.hh
@@ -1,9 +1,33 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class SetHealth
+	class SetHealth : public IPacket
 	{
+		float health;
+		int food;
+		float foodSaturation;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		float getHealth() const;
+		void setHealth(float health);
+
+		int getFood() const;
+		void setFood(int food);
+
+		float getFoodSaturation() const;
+		void setFoodSaturation(float food_saturation);
+
+		std::string toString() const override;
 	};
 }
