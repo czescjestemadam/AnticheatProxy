9a3dc318e1b3eb735487a5baa3b8db6c77ac05fa
adas <adambimek@gmail.com>
2024-07-23 00:24:50 +0200

    UseItem packet

--- src/network/protocol/packet/play/c2s/use_item.cc
+++ src/network/protocol/packet/play/c2s/use_item.cc
@@ -1,1 +1,101 @@
 #include "use_item.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::c2s::UseItem::read(const ProtocolVersion* version)
+{
+	hand = buf.readVarint();
+
+	if (*version >= ProtocolVersion::v1_19)
+		sequence = buf.readVarint();
+
+	if (*version >= ProtocolVersion::v1_21)
+	{
+		yaw = buf.readFloat();
+		pitch = buf.readFloat();
+	}
+}
+
+void acp::packet::play::c2s::UseItem::write(const ProtocolVersion* version)
+{
+	buf.writeVarint(hand);
+
+	if (*version >= ProtocolVersion::v1_19)
+		buf.writeVarint(sequence.value());
+
+	if (*version >= ProtocolVersion::v1_21)
+	{
+		buf.writeFloat(yaw.value());
+		buf.writeFloat(pitch.value());
+	}
+}
+
+bool acp::packet::play::c2s::UseItem::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::c2s::UseItem::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x39;
+	if (*version >= ProtocolVersion::v1_20_3)
+		return 0x36;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x35;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x32;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x31;
+
+	return 0x2F;
+}
+
+int acp::packet::play::c2s::UseItem::getHand() const
+{
+	return hand;
+}
+
+void acp::packet::play::c2s::UseItem::setHand(const int hand)
+{
+	this->hand = hand;
+}
+
+std::optional<int> acp::packet::play::c2s::UseItem::getSequence() const
+{
+	return sequence;
+}
+
+void acp::packet::play::c2s::UseItem::setSequence(const std::optional<int>& sequence)
+{
+	this->sequence = sequence;
+}
+
+std::optional<float> acp::packet::play::c2s::UseItem::getYaw() const
+{
+	return yaw;
+}
+
+void acp::packet::play::c2s::UseItem::setYaw(const std::optional<float>& yaw)
+{
+	this->yaw = yaw;
+}
+
+std::optional<float> acp::packet::play::c2s::UseItem::getPitch() const
+{
+	return pitch;
+}
+
+void acp::packet::play::c2s::UseItem::setPitch(const std::optional<float>& pitch)
+{
+	this->pitch = pitch;
+}
+
+std::string acp::packet::play::c2s::UseItem::toString() const
+{
+	return std::format("UseItem[hand={}, seq={}, rot={} {}]", hand, sequence.value_or(-1), yaw.value_or(-1), pitch.value_or(-1));
+}
--- src/network/protocol/packet/play/c2s/use_item.hh
+++ src/network/protocol/packet/play/c2s/use_item.hh
@@ -1,9 +1,42 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::c2s
 {
-	class UseItem
+	class UseItem : public IPacket
 	{
+		/**
+		 * - 0: main
+		 * - 1: off
+		 */
+		int hand;
+		/// 1.19 - now
+		std::optional<int> sequence;
+		/// 1.21 - now
+		std::optional<float> yaw, pitch;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		int getHand() const;
+		void setHand(int hand);
+
+		std::optional<int> getSequence() const;
+		void setSequence(const std::optional<int>& sequence);
+
+		std::optional<float> getYaw() const;
+		void setYaw(const std::optional<float>& yaw);
+
+		std::optional<float> getPitch() const;
+		void setPitch(const std::optional<float>& pitch);
+
+		std::string toString() const override;
 	};
 }
