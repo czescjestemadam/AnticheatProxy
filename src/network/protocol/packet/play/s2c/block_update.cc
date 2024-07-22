cb68015bc416f5f3c2d6fb25f52aa30056a0993f
adas <adambimek@gmail.com>
2024-07-23 01:51:09 +0200

    BlockUpdate packet

--- src/network/protocol/packet/play/s2c/block_update.cc
+++ src/network/protocol/packet/play/s2c/block_update.cc
@@ -1,1 +1,63 @@
 #include "block_update.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::BlockUpdate::read(const ProtocolVersion* version)
+{
+	blockPosition = buf.readPosition();
+	blockId = buf.readVarint();
+}
+
+void acp::packet::play::s2c::BlockUpdate::write(const ProtocolVersion* version)
+{
+	buf.writePosition(blockPosition);
+	buf.writeVarint(blockId);
+}
+
+bool acp::packet::play::s2c::BlockUpdate::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::s2c::BlockUpdate::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x09;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x0A;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x09;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x0C;
+
+	return 0x0B;
+}
+
+acp::Vec3i acp::packet::play::s2c::BlockUpdate::getBlockPosition() const
+{
+	return blockPosition;
+}
+
+void acp::packet::play::s2c::BlockUpdate::setBlockPosition(const Vec3i& block_position)
+{
+	blockPosition = block_position;
+}
+
+int acp::packet::play::s2c::BlockUpdate::getBlockId() const
+{
+	return blockId;
+}
+
+void acp::packet::play::s2c::BlockUpdate::setBlockId(const int block_id)
+{
+	blockId = block_id;
+}
+
+std::string acp::packet::play::s2c::BlockUpdate::toString() const
+{
+	return std::format("BlockUpdate[pos={} {} {}, id={}]", blockPosition.x, blockPosition.y, blockPosition.z, blockId);
+}
--- src/network/protocol/packet/play/s2c/block_update.hh
+++ src/network/protocol/packet/play/s2c/block_update.hh
@@ -1,9 +1,29 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class BlockUpdate
+	class BlockUpdate : public IPacket
 	{
+		Vec3i blockPosition;
+		int blockId;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		Vec3i getBlockPosition() const;
+		void setBlockPosition(const Vec3i& block_position);
+
+		int getBlockId() const;
+		void setBlockId(int block_id);
+
+		std::string toString() const override;
 	};
 }
