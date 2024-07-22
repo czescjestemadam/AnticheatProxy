eac7b067a316f3614b895920f0dd0e06e6e62106
adas <adambimek@gmail.com>
2024-07-22 23:55:57 +0200

    CloseContainer c2s packet

--- src/network/protocol/packet/play/c2s/close_container.cc
+++ src/network/protocol/packet/play/c2s/close_container.cc
@@ -1,1 +1,57 @@
 #include "close_container.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::c2s::CloseContainer::read(const ProtocolVersion* version)
+{
+	windowId = buf.readByte();
+}
+
+void acp::packet::play::c2s::CloseContainer::write(const ProtocolVersion* version)
+{
+	buf.writeByte(windowId);
+}
+
+bool acp::packet::play::c2s::CloseContainer::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::c2s::CloseContainer::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x0F;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x0E;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x0C;
+	if (*version >= ProtocolVersion::v1_19_3)
+		return 0x0B;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x0C;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x0B;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x09;
+
+	return 0x0A;
+}
+
+byte_t acp::packet::play::c2s::CloseContainer::getWindowId() const
+{
+	return windowId;
+}
+
+void acp::packet::play::c2s::CloseContainer::setWindowId(const byte_t window_id)
+{
+	windowId = window_id;
+}
+
+std::string acp::packet::play::c2s::CloseContainer::toString() const
+{
+	return std::format("CloseContainer[{}]", windowId);
+}
--- src/network/protocol/packet/play/c2s/close_container.hh
+++ src/network/protocol/packet/play/c2s/close_container.hh
@@ -1,9 +1,26 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::c2s
 {
-	class CloseContainer
+	class CloseContainer : public IPacket
 	{
+		/// 0 - player inventory
+		byte_t windowId;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		byte_t getWindowId() const;
+		void setWindowId(byte_t window_id);
+
+		std::string toString() const override;
 	};
 }
