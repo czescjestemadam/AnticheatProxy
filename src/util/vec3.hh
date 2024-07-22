a3418091b39470c96a2db4e9e377c475ecd3e86a
adas <adambimek@gmail.com>
2024-07-23 01:05:07 +0200

    vector

---
+++ src/util/vec3.hh
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace acp
+{
+	template<class T>
+	struct Vec3
+	{
+		T x, y, z;
+
+		Vec3 operator+(const Vec3& rhs) const
+		{
+			return { x + rhs.x, y + rhs.y, z + rhs.z };
+		}
+
+		Vec3 operator-(const Vec3& rhs) const
+		{
+			return { x - rhs.x, y - rhs.y, z - rhs.z };
+		}
+
+		Vec3 operator*(const Vec3& rhs) const
+		{
+			return { x * rhs.x, y * rhs.y, z * rhs.z };
+		}
+
+		Vec3 operator/(const Vec3& rhs) const
+		{
+			return { x / rhs.x, y / rhs.y, z / rhs.z };
+		}
+
+		bool operator==(const Vec3& rhs) const
+		{
+			return x == rhs.x && y == rhs.y && z == rhs.z;
+		}
+
+		bool operator!=(const Vec3& rhs)
+		{
+			return !(*this == rhs);
+		}
+	};
+
+	typedef Vec3<int> Vec3i;
+	typedef Vec3<float> Vec3f;
+	typedef Vec3<double> Vec3d;
+}
