cbe11cdae735dec99b8c811e2139a7803e89d48c
adas <adambimek@gmail.com>
2024-07-24 07:33:34 +0200

    PlayerAction packet

--- src/network/protocol/packet/play/c2s/player_action.cc
+++ src/network/protocol/packet/play/c2s/player_action.cc
@@ -1,1 +1,100 @@
 #include "player_action.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::c2s::PlayerAction::read(const ProtocolVersion* version)
+{
+	status = buf.readVarint();
+	blockPosition = buf.readPosition();
+	face = buf.readByte();
+
+	if (*version >= ProtocolVersion::v1_19)
+		sequence = buf.readVarint();
+}
+
+void acp::packet::play::c2s::PlayerAction::write(const ProtocolVersion* version)
+{
+	buf.writeVarint(status);
+	buf.writePosition(blockPosition);
+	buf.writeByte(face);
+
+	if (*version >= ProtocolVersion::v1_19)
+		buf.writeVarint(sequence.value());
+}
+
+bool acp::packet::play::c2s::PlayerAction::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::c2s::PlayerAction::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x24;
+	if (*version >= ProtocolVersion::v1_20_3)
+		return 0x21;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x20;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x1D;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x1C;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x1A;
+
+	return 0x1B;
+}
+
+int acp::packet::play::c2s::PlayerAction::getStatus() const
+{
+	return status;
+}
+
+void acp::packet::play::c2s::PlayerAction::setStatus(const int status)
+{
+	this->status = status;
+}
+
+acp::Vec3i acp::packet::play::c2s::PlayerAction::getBlockPosition() const
+{
+	return blockPosition;
+}
+
+void acp::packet::play::c2s::PlayerAction::setBlockPosition(const Vec3i& block_position)
+{
+	blockPosition = block_position;
+}
+
+byte_t acp::packet::play::c2s::PlayerAction::getFace() const
+{
+	return face;
+}
+
+void acp::packet::play::c2s::PlayerAction::setFace(const byte_t face)
+{
+	this->face = face;
+}
+
+const std::optional<int>& acp::packet::play::c2s::PlayerAction::getSequence() const
+{
+	return sequence;
+}
+
+void acp::packet::play::c2s::PlayerAction::setSequence(const std::optional<int>& sequence)
+{
+	this->sequence = sequence;
+}
+
+std::string acp::packet::play::c2s::PlayerAction::toString() const
+{
+	return std::format("PlayerAction[status={}, pos={} {} {}, face={}, seq={}]",
+					   status,
+					   blockPosition.x, blockPosition.y, blockPosition.z,
+					   face,
+					   sequence.value_or(-1)
+	);
+}
--- src/network/protocol/packet/play/c2s/player_action.hh
+++ src/network/protocol/packet/play/c2s/player_action.hh
@@ -1,9 +1,38 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::c2s
 {
-	class PlayerAction
+	class PlayerAction : public IPacket
 	{
+		int status;
+		Vec3i blockPosition;
+		byte_t face;
+		/// 1.19 - now
+		std::optional<int> sequence;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		int getStatus() const;
+		void setStatus(int status);
+
+		Vec3i getBlockPosition() const;
+		void setBlockPosition(const Vec3i& block_position);
+
+		byte_t getFace() const;
+		void setFace(byte_t face);
+
+		const std::optional<int>& getSequence() const;
+		void setSequence(const std::optional<int>& sequence);
+
+		std::string toString() const override;
 	};
 }
