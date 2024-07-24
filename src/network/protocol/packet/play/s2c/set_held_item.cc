43da5ca14a146db4d1c9f49d89326731a1997b57
adas <adambimek@gmail.com>
2024-07-24 07:10:06 +0200

    SetHeldItem s2c packet

--- src/network/protocol/packet/play/s2c/set_held_item.cc
+++ src/network/protocol/packet/play/s2c/set_held_item.cc
@@ -1,1 +1,59 @@
 #include "set_held_item.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::SetHeldItem::read(const ProtocolVersion* version)
+{
+	slot = buf.readByte();
+}
+
+void acp::packet::play::s2c::SetHeldItem::write(const ProtocolVersion* version)
+{
+	buf.writeByte(slot);
+}
+
+bool acp::packet::play::s2c::SetHeldItem::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::s2c::SetHeldItem::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x53;
+	if (*version >= ProtocolVersion::v1_20_3)
+		return 0x51;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x4F;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x4D;
+	if (*version >= ProtocolVersion::v1_19_3)
+		return 0x49;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x4A;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x47;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x48;
+
+	return 0x3F;
+}
+
+byte_t acp::packet::play::s2c::SetHeldItem::getSlot() const
+{
+	return slot;
+}
+
+void acp::packet::play::s2c::SetHeldItem::setSlot(byte_t slot)
+{
+	this->slot = slot;
+}
+
+std::string acp::packet::play::s2c::SetHeldItem::toString() const
+{
+	return std::format("SetHeldItem[{}]", slot);
+}
--- src/network/protocol/packet/play/s2c/set_held_item.hh
+++ src/network/protocol/packet/play/s2c/set_held_item.hh
@@ -1,9 +1,25 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class SetHeldItem
+	class SetHeldItem : public IPacket
 	{
+		byte_t slot;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		byte_t getSlot() const;
+		void setSlot(byte_t slot);
+
+		std::string toString() const override;
 	};
 }
