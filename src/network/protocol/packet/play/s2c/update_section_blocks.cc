f2482d741234a86107a1925a60a941df04461735
adas <adambimek@gmail.com>
2024-07-25 20:01:07 +0200

    UpdateSectionBlocks packet

--- src/network/protocol/packet/play/s2c/update_section_blocks.cc
+++ src/network/protocol/packet/play/s2c/update_section_blocks.cc
@@ -1,1 +1,113 @@
 #include "update_section_blocks.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::UpdateSectionBlocks::read(const ProtocolVersion* version)
+{
+	chunkSectionPosition = buf.readPosition(42, 44, 22);
+
+	if (*version <= ProtocolVersion::v1_19_4)
+		supressLightUpdates = buf.readByte();
+
+	const int len = buf.readVarint();
+	for (int i = 0; i < len; ++i)
+	{
+		const long l = buf.readVarlong();
+		blocks.emplace_back(
+			Vec3i{ static_cast<int>(l >> 8 & 0xf), static_cast<int>(l & 0xf), static_cast<int>(l >> 4 & 0xf) },
+			l >> 12
+		);
+	}
+}
+
+void acp::packet::play::s2c::UpdateSectionBlocks::write(const ProtocolVersion* version)
+{
+	buf.writePosition(chunkSectionPosition, 0x3fffff, 0xfffff, 42, 20);
+
+	if (*version <= ProtocolVersion::v1_19_4)
+		buf.writeByte(supressLightUpdates.value());
+
+	buf.writeVarint(static_cast<int>(blocks.size()));
+	for (auto& [pos, id] : blocks)
+		buf.writeVarlong(id << 12 | (pos.x << 8 | pos.z << 4 | pos.y));
+}
+
+bool acp::packet::play::s2c::UpdateSectionBlocks::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::s2c::UpdateSectionBlocks::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x49;
+	if (*version >= ProtocolVersion::v1_20_3)
+		return 0x47;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x45;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x43;
+	if (*version >= ProtocolVersion::v1_19_3)
+		return 0x3F;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x40;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x3D;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x3F;
+
+	return 0x3B;
+}
+
+acp::Vec3i& acp::packet::play::s2c::UpdateSectionBlocks::getChunkSectionPosition()
+{
+	return chunkSectionPosition;
+}
+
+const acp::Vec3i& acp::packet::play::s2c::UpdateSectionBlocks::getChunkSectionPosition() const
+{
+	return chunkSectionPosition;
+}
+
+void acp::packet::play::s2c::UpdateSectionBlocks::setChunkSectionPosition(const Vec3i& chunk_section_position)
+{
+	chunkSectionPosition = chunk_section_position;
+}
+
+const std::optional<bool>& acp::packet::play::s2c::UpdateSectionBlocks::getSupressLightUpdates() const
+{
+	return supressLightUpdates;
+}
+
+void acp::packet::play::s2c::UpdateSectionBlocks::setSupressLightUpdates(const std::optional<bool>& supress_light_updates)
+{
+	supressLightUpdates = supress_light_updates;
+}
+
+std::vector<std::pair<acp::Vec3i, int>>& acp::packet::play::s2c::UpdateSectionBlocks::getBlocks()
+{
+	return blocks;
+}
+
+const std::vector<std::pair<acp::Vec3i, int>>& acp::packet::play::s2c::UpdateSectionBlocks::getBlocks() const
+{
+	return blocks;
+}
+
+void acp::packet::play::s2c::UpdateSectionBlocks::setBlocks(const std::vector<std::pair<Vec3i, int>>& blocks)
+{
+	this->blocks = blocks;
+}
+
+std::string acp::packet::play::s2c::UpdateSectionBlocks::toString() const
+{
+	return std::format("UpdateSectionBlocks[pos={} {} {}, noLight={}, blocks={}]",
+					   chunkSectionPosition.x, chunkSectionPosition.y, chunkSectionPosition.z,
+					   supressLightUpdates.value_or(false),
+					   blocks.size()
+	);
+}
--- src/network/protocol/packet/play/s2c/update_section_blocks.hh
+++ src/network/protocol/packet/play/s2c/update_section_blocks.hh
@@ -1,9 +1,37 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class UpdateSectionBlocks
+	class UpdateSectionBlocks : public IPacket
 	{
+		Vec3i chunkSectionPosition;
+		/// x - 1.19.4
+		std::optional<bool> supressLightUpdates;
+		std::vector<std::pair<Vec3i, int>> blocks;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+
+		Vec3i& getChunkSectionPosition();
+		const Vec3i& getChunkSectionPosition() const;
+		void setChunkSectionPosition(const Vec3i& chunk_section_position);
+
+		const std::optional<bool>& getSupressLightUpdates() const;
+		void setSupressLightUpdates(const std::optional<bool>& supress_light_updates);
+
+		std::vector<std::pair<Vec3i, int>>& getBlocks();
+		const std::vector<std::pair<Vec3i, int>>& getBlocks() const;
+		void setBlocks(const std::vector<std::pair<Vec3i, int>>& blocks);
+
+		std::string toString() const override;
 	};
 }
