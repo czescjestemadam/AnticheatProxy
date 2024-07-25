63837cab83da9b746e4767f22bb5c137057fc0b1
adas <adambimek@gmail.com>
2024-07-25 19:05:28 +0200

    SetCooldown packet

--- src/network/protocol/packet/play/s2c/set_cooldown.cc
+++ src/network/protocol/packet/play/s2c/set_cooldown.cc
@@ -1,1 +1,67 @@
 #include "set_cooldown.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::SetCooldown::read(const ProtocolVersion* version)
+{
+	itemId = buf.readVarint();
+	ticks = buf.readVarint();
+}
+
+void acp::packet::play::s2c::SetCooldown::write(const ProtocolVersion* version)
+{
+	buf.writeVarint(itemId);
+	buf.writeVarint(ticks);
+}
+
+bool acp::packet::play::s2c::SetCooldown::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return false;
+}
+
+int acp::packet::play::s2c::SetCooldown::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x17;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x16;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x15;
+	if (*version >= ProtocolVersion::v1_19_3)
+		return 0x13;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x14;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x17;
+
+	return 0x16;
+}
+
+int acp::packet::play::s2c::SetCooldown::getItemId() const
+{
+	return itemId;
+}
+
+void acp::packet::play::s2c::SetCooldown::setItemId(const int item_id)
+{
+	itemId = item_id;
+}
+
+int acp::packet::play::s2c::SetCooldown::getTicks() const
+{
+	return ticks;
+}
+
+void acp::packet::play::s2c::SetCooldown::setTicks(const int ticks)
+{
+	this->ticks = ticks;
+}
+
+std::string acp::packet::play::s2c::SetCooldown::toString() const
+{
+	return std::format("SetCooldown[item={}, ticks={}]", itemId, ticks);
+}
--- src/network/protocol/packet/play/s2c/set_cooldown.hh
+++ src/network/protocol/packet/play/s2c/set_cooldown.hh
@@ -1,9 +1,29 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class SetCooldown
+	class SetCooldown : public IPacket
 	{
+		int itemId;
+		int ticks;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		bool apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		int getItemId() const;
+		void setItemId(int item_id);
+
+		int getTicks() const;
+		void setTicks(int ticks);
+
+		std::string toString() const override;
 	};
 }
