ebc99183b8f78ce3c8942a1e786247afa3b759c1
adas <adambimek@gmail.com>
2024-07-29 14:17:24 +0200

    ResetChat packet

--- src/network/protocol/packet/configuration/s2c/reset_chat.cc
+++ src/network/protocol/packet/configuration/s2c/reset_chat.cc
@@ -1,1 +1,33 @@
 #include "reset_chat.hh"
+
+#include "network/handler/configuration_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::configuration::s2c::ResetChat::read(const ProtocolVersion* version)
+{
+}
+
+void acp::packet::configuration::s2c::ResetChat::write(const ProtocolVersion* version)
+{
+}
+
+acp::HandleResult acp::packet::configuration::s2c::ResetChat::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* configHandler = dynamic_cast<ConfigurationHandler*>(handler.get()))
+		return configHandler->handle(this);
+
+	return HandleResult::FORWARD;
+}
+
+int acp::packet::configuration::s2c::ResetChat::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x06;
+
+	return -1;
+}
+
+std::string acp::packet::configuration::s2c::ResetChat::toString() const
+{
+	return "ResetChat";
+}
--- src/network/protocol/packet/configuration/s2c/reset_chat.hh
+++ src/network/protocol/packet/configuration/s2c/reset_chat.hh
@@ -1,9 +1,20 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::configuration::s2c
 {
-	class ResetChat
+	class ResetChat : public IPacket
 	{
+	public:
+		using IPacket::IPacket;
 
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		HandleResult apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		std::string toString() const override;
 	};
 }
