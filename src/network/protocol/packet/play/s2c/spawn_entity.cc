abc26d0aede8e865d473a512491bf098d66c9a60
adas <adambimek@gmail.com>
2024-07-29 12:04:57 +0200

    SpawnEntity packet

--- src/network/protocol/packet/play/s2c/spawn_entity.cc
+++ src/network/protocol/packet/play/s2c/spawn_entity.cc
@@ -1,1 +1,152 @@
 #include "spawn_entity.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::SpawnEntity::read(const ProtocolVersion* version)
+{
+	entityId = buf.readVarint();
+	entityUuid = buf.readUuid();
+	type = buf.readVarint();
+	position = buf.readVec3d();
+	yaw = buf.readAngle();
+	pitch = buf.readAngle();
+	if (*version >= ProtocolVersion::v1_19)
+		headYaw = buf.readAngle();
+	data = buf.readVarint();
+	velocity = buf.readVec3s();
+}
+
+void acp::packet::play::s2c::SpawnEntity::write(const ProtocolVersion* version)
+{
+	buf.writeVarint(entityId);
+	buf.writeUuid(entityUuid);
+	buf.writeVarint(type);
+	buf.writeVec3d(position);
+	buf.writeAngle(yaw);
+	buf.writeAngle(pitch);
+	if (*version >= ProtocolVersion::v1_19)
+		buf.writeAngle(headYaw.value());
+	buf.writeVarint(data);
+	buf.writeVec3s(velocity);
+}
+
+acp::HandleResult acp::packet::play::s2c::SpawnEntity::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return HandleResult::FORWARD;
+}
+
+int acp::packet::play::s2c::SpawnEntity::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x01;
+
+	return 0x00;
+}
+
+int acp::packet::play::s2c::SpawnEntity::getEntityId() const
+{
+	return entityId;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setEntityId(const int entity_id)
+{
+	entityId = entity_id;
+}
+
+acp::UUID acp::packet::play::s2c::SpawnEntity::getEntityUuid() const
+{
+	return entityUuid;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setEntityUuid(const UUID& entity_uuid)
+{
+	entityUuid = entity_uuid;
+}
+
+int acp::packet::play::s2c::SpawnEntity::getType() const
+{
+	return type;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setType(const int type)
+{
+	this->type = type;
+}
+
+acp::Vec3d acp::packet::play::s2c::SpawnEntity::getPosition() const
+{
+	return position;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setPosition(const Vec3d& position)
+{
+	this->position = position;
+}
+
+float acp::packet::play::s2c::SpawnEntity::getYaw() const
+{
+	return yaw;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setYaw(const float yaw)
+{
+	this->yaw = yaw;
+}
+
+float acp::packet::play::s2c::SpawnEntity::getPitch() const
+{
+	return pitch;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setPitch(const float pitch)
+{
+	this->pitch = pitch;
+}
+
+const std::optional<float>& acp::packet::play::s2c::SpawnEntity::getHeadYaw() const
+{
+	return headYaw;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setHeadYaw(const std::optional<float>& head_yaw)
+{
+	headYaw = head_yaw;
+}
+
+int acp::packet::play::s2c::SpawnEntity::getData() const
+{
+	return data;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setData(const int data)
+{
+	this->data = data;
+}
+
+acp::Vec3s acp::packet::play::s2c::SpawnEntity::getVelocity() const
+{
+	return velocity;
+}
+
+void acp::packet::play::s2c::SpawnEntity::setVelocity(const Vec3s& velocity)
+{
+	this->velocity = velocity;
+}
+
+std::string acp::packet::play::s2c::SpawnEntity::toString() const
+{
+	return std::format("SpawnEntity[id={}, uuid={}, type={}, pos={} {} {}, rot={} {}, headYaw={}, data={}, vel={} {} {}]",
+					   entityId,
+					   entityUuid.toString(),
+					   type,
+					   position.x, position.y, position.z,
+					   yaw, pitch,
+					   headYaw.value_or(-1),
+					   data,
+					   velocity.x, velocity.y, velocity.z
+	);
+}
--- src/network/protocol/packet/play/s2c/spawn_entity.hh
+++ src/network/protocol/packet/play/s2c/spawn_entity.hh
@@ -1,9 +1,57 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class SpawnEntity
+	class SpawnEntity : public IPacket
 	{
+		int entityId;
+		UUID entityUuid;
+		int type;
+		Vec3d position;
+		float yaw, pitch;
+		/// 1.19 - now
+		std::optional<float> headYaw;
+		int data;
+		Vec3s velocity;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		HandleResult apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		int getEntityId() const;
+		void setEntityId(int entity_id);
+
+		UUID getEntityUuid() const;
+		void setEntityUuid(const UUID& entity_uuid);
+
+		int getType() const;
+		void setType(int type);
+
+		Vec3d getPosition() const;
+		void setPosition(const Vec3d& position);
+
+		float getYaw() const;
+		void setYaw(float yaw);
+
+		float getPitch() const;
+		void setPitch(float pitch);
+
+		const std::optional<float>& getHeadYaw() const;
+		void setHeadYaw(const std::optional<float>& head_yaw);
+
+		int getData() const;
+		void setData(int data);
+
+		Vec3s getVelocity() const;
+		void setVelocity(const Vec3s& velocity);
+
+		std::string toString() const override;
 	};
 }
