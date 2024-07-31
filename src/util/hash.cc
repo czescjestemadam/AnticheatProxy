47cd14b5e96b226dab35da16dd4e99a1bcaea253
adas <adambimek@gmail.com>
2024-07-31 06:06:34 +0200

    some sha hashes

--- src/util/hash.cc
+++ src/util/hash.cc
@@ -1,6 +1,7 @@
 #include "hash.hh"
 
 #include <openssl/md5.h>
+#include <openssl/sha.h>
 
 std::array<byte_t, acp::hash::MD5_SIZE> acp::hash::md5(const byte_t* bytes, size_t len)
 {
@@ -12,4 +13,30 @@ std::array<byte_t, acp::hash::MD5_SIZE> acp::hash::md5(const byte_t* bytes, size
 std::array<byte_t, acp::hash::MD5_SIZE> acp::hash::md5(const ByteBuf& buf)
 {
 	return md5(buf.data(), buf.size());
+}
+
+
+std::array<byte_t, acp::hash::SHA1_SIZE> acp::hash::sha1(const byte_t* bytes, size_t len)
+{
+	byte_t out[SHA1_SIZE];
+	SHA1(bytes, len, out);
+	return std::to_array(out);
+}
+
+std::array<byte_t, acp::hash::SHA1_SIZE> acp::hash::sha1(const ByteBuf& buf)
+{
+	return sha1(buf.data(), buf.size());
+}
+
+
+std::array<byte_t, acp::hash::SHA256_SIZE> acp::hash::sha256(const byte_t* bytes, size_t len)
+{
+	byte_t out[SHA256_SIZE];
+	SHA256(bytes, len, out);
+	return std::to_array(out);
+}
+
+std::array<byte_t, acp::hash::SHA256_SIZE> acp::hash::sha256(const ByteBuf& buf)
+{
+	return sha256(buf.data(), buf.size());
 }
--- src/util/hash.hh
+++ src/util/hash.hh
@@ -9,4 +9,16 @@ namespace acp::hash
 
 	std::array<byte_t, MD5_SIZE> md5(const byte_t* bytes, size_t len);
 	std::array<byte_t, MD5_SIZE> md5(const ByteBuf& buf);
+
+
+	constexpr size_t SHA1_SIZE = 40;
+
+	std::array<byte_t, SHA1_SIZE> sha1(const byte_t* bytes, size_t len);
+	std::array<byte_t, SHA1_SIZE> sha1(const ByteBuf& buf);
+
+
+	constexpr size_t SHA256_SIZE = 64;
+
+	std::array<byte_t, SHA256_SIZE> sha256(const byte_t* bytes, size_t len);
+	std::array<byte_t, SHA256_SIZE> sha256(const ByteBuf& buf);
 }
