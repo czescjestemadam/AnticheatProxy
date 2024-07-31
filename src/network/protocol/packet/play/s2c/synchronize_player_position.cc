6a39c1654c7b80e8e0a8269f4a81aacaec9ae970
adas <adambimek@gmail.com>
2024-07-31 17:22:42 +0200

    SynchronizePlayerPosition packet

--- src/network/protocol/packet/play/s2c/synchronize_player_position.cc
+++ src/network/protocol/packet/play/s2c/synchronize_player_position.cc
@@ -1,1 +1,127 @@
 #include "synchronize_player_position.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::SynchronizePlayerPosition::read(const ProtocolVersion* version)
+{
+	position = buf.readVec3d();
+	yaw = buf.readFloat();
+	pitch = buf.readFloat();
+	flags = buf.readByte();
+	teleportId = buf.readVarint();
+
+	if (*version >= ProtocolVersion::v1_17 && *version <= ProtocolVersion::v1_19_3)
+		dismount = buf.readByte();
+}
+
+void acp::packet::play::s2c::SynchronizePlayerPosition::write(const ProtocolVersion* version)
+{
+	buf.writeVec3d(position);
+	buf.writeFloat(yaw);
+	buf.writeFloat(pitch);
+	buf.writeByte(flags);
+	buf.writeVarint(teleportId);
+
+	if (*version >= ProtocolVersion::v1_17 && *version <= ProtocolVersion::v1_19_3)
+		buf.writeByte(dismount.value());
+}
+
+acp::HandleResult acp::packet::play::s2c::SynchronizePlayerPosition::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return HandleResult::FORWARD;
+}
+
+int acp::packet::play::s2c::SynchronizePlayerPosition::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x40;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x3E;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x3C;
+	if (*version >= ProtocolVersion::v1_19_3)
+		return 0x38;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x39;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x36;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x38;
+
+	return 0x34;
+}
+
+acp::Vec3d& acp::packet::play::s2c::SynchronizePlayerPosition::getPosition()
+{
+	return position;
+}
+
+void acp::packet::play::s2c::SynchronizePlayerPosition::setPosition(const Vec3d& position)
+{
+	this->position = position;
+}
+
+float acp::packet::play::s2c::SynchronizePlayerPosition::getYaw() const
+{
+	return yaw;
+}
+
+void acp::packet::play::s2c::SynchronizePlayerPosition::setYaw(const float yaw)
+{
+	this->yaw = yaw;
+}
+
+float acp::packet::play::s2c::SynchronizePlayerPosition::getPitch() const
+{
+	return pitch;
+}
+
+void acp::packet::play::s2c::SynchronizePlayerPosition::setPitch(const float pitch)
+{
+	this->pitch = pitch;
+}
+
+byte_t acp::packet::play::s2c::SynchronizePlayerPosition::getFlags() const
+{
+	return flags;
+}
+
+void acp::packet::play::s2c::SynchronizePlayerPosition::setFlags(const byte_t flags)
+{
+	this->flags = flags;
+}
+
+int acp::packet::play::s2c::SynchronizePlayerPosition::getTeleportId() const
+{
+	return teleportId;
+}
+
+void acp::packet::play::s2c::SynchronizePlayerPosition::setTeleportId(const int teleport_id)
+{
+	teleportId = teleport_id;
+}
+
+std::optional<bool>& acp::packet::play::s2c::SynchronizePlayerPosition::getDismount()
+{
+	return dismount;
+}
+
+void acp::packet::play::s2c::SynchronizePlayerPosition::setDismount(const std::optional<bool>& dismount)
+{
+	this->dismount = dismount;
+}
+
+std::string acp::packet::play::s2c::SynchronizePlayerPosition::toString() const
+{
+	return std::format("SynchronizePlayerPosition[pos={} {} {}, rot={} {}, fl={:02x}, id={}, dismount={}]",
+					   position.x, position.y, position.z,
+					   yaw, pitch,
+					   flags,
+					   teleportId,
+					   dismount.value_or(false)
+	);
+}
--- src/network/protocol/packet/play/s2c/synchronize_player_position.hh
+++ src/network/protocol/packet/play/s2c/synchronize_player_position.hh
@@ -1,9 +1,56 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class SynchronizePlayerPosition
+	class SynchronizePlayerPosition : public IPacket
 	{
+		/// field absolute if flags == 0
+		Vec3d position;
+		/// field absolute if flags == 0
+		float yaw, pitch;
 
+		/// position flags
+		/// - x: 0x01
+		/// - y: 0x02
+		/// - z: 0x04
+		/// - pitch: 0x08
+		/// - yaw: 0x10
+		byte_t flags;
+
+		int teleportId;
+
+		/// 1.17 - 1.19.3
+		std::optional<bool> dismount;
+
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		HandleResult apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		Vec3d& getPosition();
+		void setPosition(const Vec3d& position);
+
+		float getYaw() const;
+		void setYaw(float yaw);
+
+		float getPitch() const;
+		void setPitch(float pitch);
+
+		byte_t getFlags() const;
+		void setFlags(byte_t flags);
+
+		int getTeleportId() const;
+		void setTeleportId(int teleport_id);
+
+		std::optional<bool>& getDismount();
+		void setDismount(const std::optional<bool>& dismount);
+
+		std::string toString() const override;
 	};
 }
