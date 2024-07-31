7701f785d02602595bfd05f76ee0c277a24a4120
adas <adambimek@gmail.com>
2024-07-31 15:31:45 +0200

    KeepAlive config c2s packet

--- src/network/protocol/packet/configuration/c2s/keep_alive.cc
+++ src/network/protocol/packet/configuration/c2s/keep_alive.cc
@@ -1,1 +1,47 @@
 #include "keep_alive.hh"
+
+#include "network/handler/configuration_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::configuration::c2s::KeepAlive::read(const ProtocolVersion* version)
+{
+	keepAliveId = buf.readLong();
+}
+
+void acp::packet::configuration::c2s::KeepAlive::write(const ProtocolVersion* version)
+{
+	buf.writeLong(keepAliveId);
+}
+
+acp::HandleResult acp::packet::configuration::c2s::KeepAlive::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* configHandler = dynamic_cast<ConfigurationHandler*>(handler.get()))
+		return configHandler->handle(this);
+
+	return HandleResult::FORWARD;
+}
+
+int acp::packet::configuration::c2s::KeepAlive::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x04;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x03;
+
+	return -1;
+}
+
+long acp::packet::configuration::c2s::KeepAlive::getKeepAliveId() const
+{
+	return keepAliveId;
+}
+
+void acp::packet::configuration::c2s::KeepAlive::setKeepAliveId(const long keep_alive_id)
+{
+	keepAliveId = keep_alive_id;
+}
+
+std::string acp::packet::configuration::c2s::KeepAlive::toString() const
+{
+	return std::format("KeepAlive[{}]", keepAliveId);
+}
--- src/network/protocol/packet/configuration/c2s/keep_alive.hh
+++ src/network/protocol/packet/configuration/c2s/keep_alive.hh
@@ -1,9 +1,25 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::configuration::c2s
 {
-	class KeepAlive
+	class KeepAlive : public IPacket
 	{
+		long keepAliveId;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		HandleResult apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		long getKeepAliveId() const;
+		void setKeepAliveId(long keep_alive_id);
+
+		std::string toString() const override;
 	};
 }
