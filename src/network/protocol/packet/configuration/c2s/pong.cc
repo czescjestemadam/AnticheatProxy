56bba1f394492c55e1266c868230e742c887d5ed
adas <adambimek@gmail.com>
2024-07-31 16:23:18 +0200

    Pong config packet

--- src/network/protocol/packet/configuration/c2s/pong.cc
+++ src/network/protocol/packet/configuration/c2s/pong.cc
@@ -1,1 +1,47 @@
 #include "pong.hh"
+
+#include "network/handler/configuration_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::configuration::c2s::Pong::read(const ProtocolVersion* version)
+{
+	pongId = buf.readInt();
+}
+
+void acp::packet::configuration::c2s::Pong::write(const ProtocolVersion* version)
+{
+	buf.writeInt(pongId);
+}
+
+acp::HandleResult acp::packet::configuration::c2s::Pong::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* configHandler = dynamic_cast<ConfigurationHandler*>(handler.get()))
+		return configHandler->handle(this);
+
+	return acp::HandleResult::FORWARD;
+}
+
+int acp::packet::configuration::c2s::Pong::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x05;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x04;
+
+	return -1;
+}
+
+int acp::packet::configuration::c2s::Pong::getPongId() const
+{
+	return pongId;
+}
+
+void acp::packet::configuration::c2s::Pong::setPongId(const int pong_id)
+{
+	pongId = pong_id;
+}
+
+std::string acp::packet::configuration::c2s::Pong::toString() const
+{
+	return std::format("Pong[{}]", pongId);
+}
--- src/network/protocol/packet/configuration/c2s/pong.hh
+++ src/network/protocol/packet/configuration/c2s/pong.hh
@@ -1,9 +1,25 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::configuration::c2s
 {
-	class Pong
+	class Pong : public IPacket
 	{
+		int pongId;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		HandleResult apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		int getPongId() const;
+		void setPongId(int pong_id);
+
+		std::string toString() const override;
 	};
 }
