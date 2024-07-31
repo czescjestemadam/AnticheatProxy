854d2d7fb0da0b5cd3a7169eb072a4136a05c233
adas <adambimek@gmail.com>
2024-07-31 17:31:03 +0200

    CloseContainer s2c packet

--- src/network/protocol/packet/play/s2c/close_container.cc
+++ src/network/protocol/packet/play/s2c/close_container.cc
@@ -1,1 +1,53 @@
 #include "close_container.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::CloseContainer::read(const ProtocolVersion* version)
+{
+	windowId = buf.readByte();
+}
+
+void acp::packet::play::s2c::CloseContainer::write(const ProtocolVersion* version)
+{
+	buf.writeByte(windowId);
+}
+
+acp::HandleResult acp::packet::play::s2c::CloseContainer::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return HandleResult::FORWARD;
+}
+
+int acp::packet::play::s2c::CloseContainer::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x12;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x11;
+	if (*version >= ProtocolVersion::v1_19_3)
+		return 0x0F;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x10;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x13;
+
+	return 0x12;
+}
+
+byte_t acp::packet::play::s2c::CloseContainer::getWindowId() const
+{
+	return windowId;
+}
+
+void acp::packet::play::s2c::CloseContainer::setWindowId(const byte_t window_id)
+{
+	windowId = window_id;
+}
+
+std::string acp::packet::play::s2c::CloseContainer::toString() const
+{
+	return std::format("CloseContainer[{}]", windowId);
+}
--- src/network/protocol/packet/play/s2c/close_container.hh
+++ src/network/protocol/packet/play/s2c/close_container.hh
@@ -1,9 +1,26 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class CloseContainer
+	class CloseContainer : public IPacket
 	{
+		/// 0 = inventory
+		byte_t windowId;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		HandleResult apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		byte_t getWindowId() const;
+		void setWindowId(byte_t window_id);
+
+		std::string toString() const override;
 	};
 }
