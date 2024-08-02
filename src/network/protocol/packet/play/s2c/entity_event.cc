525c76ebfb3c4e4f585c805b037f1efa9a75043c
adas <adambimek@gmail.com>
2024-08-02 11:40:45 +0200

    EntityEvent packet

--- src/network/protocol/packet/play/s2c/entity_event.cc
+++ src/network/protocol/packet/play/s2c/entity_event.cc
@@ -1,1 +1,69 @@
 #include "entity_event.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::EntityEvent::read(const ProtocolVersion* version)
+{
+	entityId = buf.readInt();
+	status = buf.readByte();
+}
+
+void acp::packet::play::s2c::EntityEvent::write(const ProtocolVersion* version)
+{
+	buf.writeInt(entityId);
+	buf.writeByte(status);
+}
+
+acp::HandleResult acp::packet::play::s2c::EntityEvent::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return HandleResult::FORWARD;
+}
+
+int acp::packet::play::s2c::EntityEvent::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_5)
+		return 0x1F;
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x1D;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x1C;
+	if (*version >= ProtocolVersion::v1_19_3)
+		return 0x19;
+	if (*version >= ProtocolVersion::v1_19_1)
+		return 0x1A;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x18;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x1B;
+
+	return 0x1A;
+}
+
+int acp::packet::play::s2c::EntityEvent::getEntityId() const
+{
+	return entityId;
+}
+
+void acp::packet::play::s2c::EntityEvent::setEntityId(const int entity_id)
+{
+	entityId = entity_id;
+}
+
+byte_t acp::packet::play::s2c::EntityEvent::getStatus() const
+{
+	return status;
+}
+
+void acp::packet::play::s2c::EntityEvent::setStatus(const byte_t status)
+{
+	this->status = status;
+}
+
+std::string acp::packet::play::s2c::EntityEvent::toString() const
+{
+	return std::format("EntityEvent[id={}, status={}]", entityId, status);
+}
--- src/network/protocol/packet/play/s2c/entity_event.hh
+++ src/network/protocol/packet/play/s2c/entity_event.hh
@@ -1,9 +1,29 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class EntityEvent
+	class EntityEvent : public IPacket
 	{
+		int entityId;
+		byte_t status;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		HandleResult apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		int getEntityId() const;
+		void setEntityId(int entity_id);
+
+		byte_t getStatus() const;
+		void setStatus(byte_t status);
+
+		std::string toString() const override;
 	};
 }
