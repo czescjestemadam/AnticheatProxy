5d51b5a1a8613aa36d42dce3857884a0570f9eb4
adas <adambimek@gmail.com>
2024-08-02 17:01:27 +0200

    BlockEntityData packet

--- src/network/protocol/packet/play/s2c/block_entity_data.cc
+++ src/network/protocol/packet/play/s2c/block_entity_data.cc
@@ -1,1 +1,87 @@
 #include "block_entity_data.hh"
+
+#include "network/handler/play_handler.hh"
+#include "network/protocol/protocol_version.hh"
+
+void acp::packet::play::s2c::BlockEntityData::read(const ProtocolVersion* version)
+{
+	position = buf.readPosition();
+	type = *version < ProtocolVersion::v1_18 ? buf.readByte() : buf.readVarint();
+	data = buf.readNbt(*version < ProtocolVersion::v1_20_2);
+}
+
+void acp::packet::play::s2c::BlockEntityData::write(const ProtocolVersion* version)
+{
+	buf.writePosition(position);
+	if (*version < ProtocolVersion::v1_18)
+		buf.writeByte(type);
+	else
+		buf.writeVarint(type);
+	buf.writeNbt(data, *version < ProtocolVersion::v1_20_2);
+}
+
+acp::HandleResult acp::packet::play::s2c::BlockEntityData::apply(std::unique_ptr<INetworkHandler>& handler)
+{
+	if (auto* playHandler = dynamic_cast<PlayHandler*>(handler.get()))
+		return playHandler->handle(this);
+
+	return HandleResult::FORWARD;
+}
+
+int acp::packet::play::s2c::BlockEntityData::getId(const ProtocolVersion* version) const
+{
+	if (*version >= ProtocolVersion::v1_20_2)
+		return 0x07;
+	if (*version >= ProtocolVersion::v1_19_4)
+		return 0x08;
+	if (*version >= ProtocolVersion::v1_19)
+		return 0x07;
+	if (*version >= ProtocolVersion::v1_17)
+		return 0x0A;
+
+	return 0x09;
+}
+
+const acp::Vec3i& acp::packet::play::s2c::BlockEntityData::getPosition() const
+{
+	return position;
+}
+
+void acp::packet::play::s2c::BlockEntityData::setPosition(const Vec3i& position)
+{
+	this->position = position;
+}
+
+int acp::packet::play::s2c::BlockEntityData::getType() const
+{
+	return type;
+}
+
+void acp::packet::play::s2c::BlockEntityData::setType(const int type)
+{
+	this->type = type;
+}
+
+std::unique_ptr<acp::nbt::Tag>& acp::packet::play::s2c::BlockEntityData::getData()
+{
+	return data;
+}
+
+const std::unique_ptr<acp::nbt::Tag>& acp::packet::play::s2c::BlockEntityData::getData() const
+{
+	return data;
+}
+
+void acp::packet::play::s2c::BlockEntityData::setData(std::unique_ptr<nbt::Tag>&& data)
+{
+	this->data = std::move(data);
+}
+
+std::string acp::packet::play::s2c::BlockEntityData::toString() const
+{
+	return std::format("BlockEntityData[pos={} {} {}, type={}, data={}]",
+					   position.x, position.y, position.z,
+					   type,
+					   data->toString()
+	);
+}
--- src/network/protocol/packet/play/s2c/block_entity_data.hh
+++ src/network/protocol/packet/play/s2c/block_entity_data.hh
@@ -1,9 +1,49 @@
 #pragma once
+#include "network/protocol/packet/i_packet.hh"
 
 namespace acp::packet::play::s2c
 {
-	class BlockEntityData
+	class BlockEntityData : public IPacket
 	{
+		Vec3i position;
+		/// Action field (pvn < 1.18):
+		/// - @c 1: Set data of a mob spawner (everything except for SpawnPotentials: current delay, min/max delay, mob to be spawned, spawn count, spawn range, etc.)
+		/// - @c 2: Set command block text (command and last execution status)
+		/// - @c 3: Set the level, primary, and secondary powers of a beacon
+		/// - @c 4: Set rotation and skin of mob head
+		/// - @c 5: Declare a conduit
+		/// - @c 6: Set base color and patterns on a banner
+		/// - @c 7: Set the data for a Structure tile entity
+		/// - @c 8: Set the destination for a end gateway
+		/// - @c 9: Set the text on a sign
+		/// - @c 10: Unused
+		/// - @c 11: Declare a bed
+		/// - @c 12: Set data of a jigsaw block
+		/// - @c 13: Set items in a campfire
+		/// - @c 14: Beehive information
+		int type;
+		std::unique_ptr<nbt::Tag> data;
 
+	public:
+		using IPacket::IPacket;
+
+		void read(const ProtocolVersion* version) override;
+		void write(const ProtocolVersion* version) override;
+
+		HandleResult apply(std::unique_ptr<INetworkHandler>& handler) override;
+
+		int getId(const ProtocolVersion* version) const override;
+
+		const Vec3i& getPosition() const;
+		void setPosition(const Vec3i& position);
+
+		int getType() const;
+		void setType(int type);
+
+		std::unique_ptr<nbt::Tag>& getData();
+		const std::unique_ptr<nbt::Tag>& getData() const;
+		void setData(std::unique_ptr<nbt::Tag>&& data);
+
+		std::string toString() const override;
 	};
 }
