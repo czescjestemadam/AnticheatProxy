f52eb27021b73c59417fe9d2df06bef7ca785b42
adas <adambimek@gmail.com>
2024-08-06 20:45:18 +0200

    fix template, add function for entity bounding boxes

--- src/util/bounding_box.hh
+++ src/util/bounding_box.hh
@@ -8,11 +8,17 @@ namespace acp
 	{
 		Vec3<T> min, max;
 
-		template<T>
 		static BoundingBox from(Vec3<T> center, Vec3<T> size)
 		{
 			const Vec3<T> halfSize = size / 2;
 			return { center - halfSize, center + halfSize };
+		}
+
+		static BoundingBox fromPos(Vec3<T> pos, Vec3<T> size)
+		{
+			const Vec3<T> halfSize = size / 2;
+			const Vec3<T> min = { pos.x - halfSize.x, pos.y, pos.z - halfSize.z };
+			return { min, min + size };
 		}
 	};
 
