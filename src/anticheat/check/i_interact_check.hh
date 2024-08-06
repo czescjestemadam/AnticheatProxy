8ec2d8191e9de041e4e76b04f8df427880e6ec9e
adas <adambimek@gmail.com>
2024-08-06 03:02:15 +0200

    checks

--- src/anticheat/check/i_interact_check.hh
+++ src/anticheat/check/i_interact_check.hh
@@ -1,11 +1,24 @@
 #pragma once
 #include "i_check.hh"
+#include "network/protocol/packet/play/c2s/interact.hh"
+#include "network/protocol/packet/play/c2s/player_action.hh"
+#include "network/protocol/packet/play/c2s/swing_arm.hh"
+#include "network/protocol/packet/play/c2s/use_item.hh"
+#include "network/protocol/packet/play/c2s/use_item_on.hh"
 
 namespace acp
 {
 	class IInteractCheck : public ICheck
 	{
 	public:
+		virtual void check(packet::play::c2s::Interact* packet) = 0;
 
+		virtual void check(packet::play::c2s::UseItem* packet) = 0;
+
+		virtual void check(packet::play::c2s::UseItemOn* packet) = 0;
+
+		virtual void check(packet::play::c2s::PlayerAction* packet) = 0;
+
+		virtual void check(packet::play::c2s::SwingArm* packet) = 0;
 	};
 }
--- src/anticheat/check/i_movement_check.hh
+++ src/anticheat/check/i_movement_check.hh
@@ -1,11 +1,30 @@
 #pragma once
 #include "i_check.hh"
+#include "network/protocol/packet/play/c2s/move_vehicle.hh"
+#include "network/protocol/packet/play/c2s/player_command.hh"
+#include "network/protocol/packet/play/c2s/player_input.hh"
+#include "network/protocol/packet/play/c2s/set_player_on_ground.hh"
+#include "network/protocol/packet/play/c2s/set_player_position.hh"
+#include "network/protocol/packet/play/c2s/set_player_position_rotation.hh"
+#include "network/protocol/packet/play/c2s/set_player_rotation.hh"
 
 namespace acp
 {
 	class IMovementCheck : public ICheck
 	{
 	public:
+		virtual void check(packet::play::c2s::SetPlayerPosition* packet) = 0;
 
+		virtual void check(packet::play::c2s::SetPlayerPositionRotation* packet) = 0;
+
+		virtual void check(packet::play::c2s::SetPlayerRotation* packet) = 0;
+
+		virtual void check(packet::play::c2s::SetPlayerOnGround* packet) = 0;
+
+		virtual void check(packet::play::c2s::MoveVehicle* packet) = 0;
+
+		virtual void check(packet::play::c2s::PlayerInput* packet) = 0;
+
+		virtual void check(packet::play::c2s::PlayerCommand* packet) = 0;
 	};
 }
