cb1e88b4c1856cc212a473c7ce48efa70e879460
adas <adambimek@gmail.com>
2024-08-08 13:15:11 +0200

    program start time global var

--- src/globals.hh
+++ src/globals.hh
@@ -4,4 +4,5 @@ namespace acp::globals
 {
 	constexpr const char* VERSION = "b0.1";
 	constexpr const char* COMPILE_TIME = __DATE__ " " __TIME__;
+	const std::chrono::system_clock::time_point STARTED_AT = std::chrono::system_clock::now();
 }
