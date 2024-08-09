a006abe9964a0235424f156247de3bb23ff94d2e
adas <adambimek@gmail.com>
2024-08-09 21:52:38 +0200

    include file count

--- lines.sh
+++ lines.sh
@@ -1,3 +1,14 @@
 #!/bin/fish
 
+echo -n "files: "
+fd -tf . src/ | wc -l
+
+echo -n "source files: "
+fd -tf . src/ -e cc | wc -l
+
+echo -n "header files: "
+fd -tf . src/ -e hh | wc -l
+echo
+
+echo "lines words chars"
 cat (fd -tf . src/) | wc
